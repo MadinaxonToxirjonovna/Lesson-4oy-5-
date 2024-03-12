31bfd8c3a525529f78c2a235b3bb71f085ad533b
Madinaxon Toxirjonovna <madinaxontoxirjonovna@gamil.com>
2024-03-12 22:26:27 +0500

    Lesson 4oy(5)

---
+++ main.js
@@ -0,0 +1,115 @@
+                       // UY ISHI
+
+// let random = Math.floor(Math.random() * 100 + 1)
+
+// alert('Randomda chiqqan son: ' + random)
+
+// if(random % 3 == 0 && random % 5 == 0){
+//     alert(random + ": fizzbuzz")
+// }else if(random % 3 == 0){
+//     alert(random + ": fizz" )
+// }else if(random % 5 == 0){
+//     alert(random + ": buzz")
+// }else{
+//     alert(random + " soni fizz ham buzz ham fizzbuzz ham emas")
+// }
+
+
+
+//                             1 masala
+// let a = Number(prompt("Enter a= "));
+
+// let p;
+// p = 4 * a;
+// alert("P = " + P)
+
+                        //  2 masala
+// let a = Number(prompt("Enter a= "));
+
+// let S;
+// S = Math.pow(a, 2);
+// alert("s = " + S)
+
+// let a = Number(prompt("Enter a, b"));
+
+// let S;
+// let P;
+                    //  3 masala
+// let a = +prompt('a songa qiymat kiriting');
+// let b = +prompt('b soniga qiymat kiritng');
+// let S= a*b;
+// let p = 2*(a+b);
+// alert(S);
+// alert(p);
+
+            //    4masala
+// let d = +prompt('Biron bir son kiriting');
+// let s =Math.floor(Math.PI * d);
+// alert(s);
+
+                // 5 masala
+// let a = +prompt("Son");
+// let c = Math.pow(a, 3)
+// alert(c);
+
+
+                //   6 masala
+//  let a  = +prompt("a ni kiritng")
+//  let b  = +prompt("b ni kiritng")
+//  let c  = +prompt("c ni kiritng")
+
+// let V, S;
+// S = 2*(a*b+b*c+a*c);
+// V = a*b*c;
+// alert('V = ' + V +"\n S =" + S);
+
+
+                //    7 masala
+// let R = +prompt("Doiraning radiusi")
+// let L = +prompt("Uzunligi")
+// let S = +prompt("Yuzasi")
+
+// L = 2 * Math.PI*R;
+// S = Math.PI*Math.pow(R,2);
+// alert("R = "+R + "\n L ="+L + "\n S = " + S);
+
+                                    //  8 masala
+// let a = +prompt("a")
+// let b = +prompt("b")
+// alert((a+b)/2);
+
+                                //   9 masala
+// let a = +prompt("a")
+// let b = +prompt("b")
+// alert(Math.sqrt (a*b))
+
+
+                                                //    Sinf ishi
+// let text = "Maxmudova";
+// let uzunligi = text.length;
+// alert(uzunligi)
+
+
+//  let text1 = "Hello Mars!";
+//  let text2 = text1.toLowerCase();
+//  alert(text2)
+
+ 
+//  let text4 = "Hello world!";
+//  let text3 = text1.toUpperCase();
+//  alert(text3)
+
+
+                                                //    Uy ishi
+
+let name = prompt("Ismingizni kiriting");
+let name1 = prompt("Bironta harf kiriting")
+let test = name.includes(name1)
+
+
+if (test) {
+    alert(name + " ismining ichida " + name1 +  " harf mavjud ")
+} else {
+    alert(name + " ismining ichida " + name1 +  " harf mavjud emas ")
+}
+
